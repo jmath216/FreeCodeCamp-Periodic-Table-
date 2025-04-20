572698e02744e588eb0637137cc0c7fbbcd0a4f1
John Mathew <jblfortnite@outlook.com>
2025-04-20 04:35:23 +0000

    feat: added function to query database and retrieve element data

--- element.sh
+++ element.sh
@@ -3,3 +3,12 @@
 # Display information and query the database
 get_element_info() {
     local input="$1"
+
+        # Query for atomic number (integer), symbol (string), and name (string)
+    result=$(psql -U your_db_user -d your_db_name -t -c "
+        SELECT atomic_number, symbol, name, type, atomic_mass, melting_point_celsius, boiling_point_celsius
+        FROM elements
+        LEFT JOIN properties ON elements.atomic_number = properties.atomic_number
+        WHERE elements.atomic_number = '${input}' 
+        OR elements.symbol = '${input}' 
+        OR elements.name = '${input}'")
